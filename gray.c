1b84c9a9d3fe9ab684bf2b58f3be8d05ee30a21e
ColdRiver <17830831606@163.com>
2025-07-30 10:51:48 +0800

    删除不必要功能

--- gray.c
+++ gray.c
@@ -4,18 +4,13 @@
 
 #include "stdint.h"
 
-/*
-根据灰度二进制计算出偏差
-*/
 float gray_calculate_bias(uint8_t sensor_data)
 {
 	static float error = 0;
 	switch(sensor_data)
 	{
-		// 0个0的情况
-		case 0b11111111: // 没扫到黑线，不作任何处理，沿用上次结果
+		case 0b11111111:
 			break;
-		// 1个0的情况
 		case 0b01111111: 
 			error = -3.5;
 			break;
@@ -40,7 +35,6 @@ float gray_calculate_bias(uint8_t sensor_data)
 		case 0b11111110:
 			error = 3.5;
 			break;
-		// 2个0的情况
 		case 0b00111111:
 			error = -3;
 			break;
@@ -62,7 +56,6 @@ float gray_calculate_bias(uint8_t sensor_data)
 		case 0b11111100:
 			error = 3;
 			break;
-		// 3个0的情况
 		case 0b00011111:
 			error = -2.5;
 			break;
@@ -81,7 +74,6 @@ float gray_calculate_bias(uint8_t sensor_data)
 		case 0b11111000:
 			error = 2.5;
 			break;
-		// 4个0的情况
 		case 0b00001111:
 			error = -2;
 			break;
@@ -97,7 +89,6 @@ float gray_calculate_bias(uint8_t sensor_data)
 		case 0b11110000:
 			error = 2;
 			break;
-		// 5个0的情况
 		case 0b00000111:
 			error = -1.5;
 			break;
@@ -110,7 +101,6 @@ float gray_calculate_bias(uint8_t sensor_data)
 		case 0b11100000:
 			error = 1.5;
 			break;
-		// 6个0的情况
 		case 0b00000011:
 			error = -1;
 			break;
@@ -120,22 +110,15 @@ float gray_calculate_bias(uint8_t sensor_data)
 		case 0b11000000:
 			error = 1;
 			break;
-		// 7个0的情况
 		case 0b00000001:
 			error = -0.5;
 			break;
 		case 0b10000000:
 			error = 0.5;
 			break;
-		// 8个0的情况
-		case 0b00000000: // 全是黑线，走直线
+		case 0b00000000:
 			error = 0;
 			break;
 	}
 	return error;
-}
-
-void gray_calculate_pid()
-{
-	
 }
--- pid.c
+++ pid.c
@@ -7,24 +7,15 @@
 float PID_calculate(PID * pid,float input,float target)
 {
 	float error = target - input;
-	
-	float P = pid->Kp *  error;// 比例
-	
-	pid->I += pid->Ki * error;// 积分累加
-	// 积分限幅
+	float P = pid->Kp * error;
+	pid->I += pid->Ki * error;
 	pid->I = pid->I > pid->I_max ? pid->I_max : pid->I;
 	pid->I = pid->I < pid->I_min ? pid->I_min : pid->I;
-	
-	float D = pid->Kd * (error - pid->prev_error); // 微分
-	
-	
-	pid->prev_error = error; // 误差传递
-	
+	float D = pid->Kd * (error - pid->prev_error);
+	pid->prev_error = error;
 	float output = P + pid->I + D;
-	
 	output = output > pid->max ? pid->max : output;
 	output = output < pid->min ? pid->min : output;
-	
 	return output;
 }
 
@@ -33,3 +24,15 @@ float PID_clear(PID * pid)
 	pid->I = 0;
 	pid->prev_error = 0;
 }
+
+PID line_patrol_pid = {
+	.Kp = 4,
+	.Ki = 2,
+	.Kd = 0.9,
+	.prev_error = 0,
+	.I = 0,
+	.I_max = 300,
+	.I_min = -300,
+	.max = 500,
+	.min = -500,
+};
