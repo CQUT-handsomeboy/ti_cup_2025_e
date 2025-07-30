af476c179be2e5e66f9a3905861cb53e410350cb
Xiao Shuai <1945810700@qq.com>
2025-07-30 16:01:26 +0800

    配置波特率1000000

--- ti_msp_dl_config.h
+++ ti_msp_dl_config.h
@@ -132,9 +132,9 @@ extern "C" {
 #define GPIO_UART_0_IOMUX_TX                                     (IOMUX_PINCM15)
 #define GPIO_UART_0_IOMUX_RX_FUNC                      IOMUX_PINCM14_PF_UART1_RX
 #define GPIO_UART_0_IOMUX_TX_FUNC                      IOMUX_PINCM15_PF_UART1_TX
-#define UART_0_BAUD_RATE                                                  (9600)
-#define UART_0_IBRD_32_MHZ_9600_BAUD                                       (208)
-#define UART_0_FBRD_32_MHZ_9600_BAUD                                        (21)
+#define UART_0_BAUD_RATE                                               (1000000)
+#define UART_0_IBRD_32_MHZ_1000000_BAUD                                      (2)
+#define UART_0_FBRD_32_MHZ_1000000_BAUD                                      (0)
 
 
 
