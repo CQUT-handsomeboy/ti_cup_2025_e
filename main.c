c2fabd9d555ec22b46759d61e63a47369a8dfe5b
ColdRiver <17830831606@163.com>
2025-07-31 18:06:59 +0800

    取消注释

--- main.c
+++ main.c
@@ -23,18 +23,18 @@
 // In order to achieve a turn, 
 // the difference is added or subtracted based on this speed.
 
-//#define BASE_CTRL 550
-//#define BASE_SPEED 550
-//#define INC_SPEED	200
+#define BASE_CTRL 550
+#define BASE_SPEED 550
+#define INC_SPEED	200
 
-//void usart0_send_bytes(unsigned char *buf, int len)
-//{
-//	while (len--)
-//	{
-//		DL_UART_Main_transmitDataBlocking(UART_0_INST, *buf);
-//		buf++;
-//	}
-//}
+void usart0_send_bytes(unsigned char *buf, int len)
+{
+	while (len--)
+	{
+		DL_UART_Main_transmitDataBlocking(UART_0_INST, *buf);
+		buf++;
+	}
+}
 
 uint32_t l_encoder_l_count = 0;
 uint32_t l_last_encoder_count = 0;
@@ -43,7 +43,7 @@ uint32_t r_encoder_l_count = 0;
 uint32_t r_last_encoder_count = 0;
 uint32_t r_encoder_speed = 0;
 
-//#define DELAY (3200000)
+#define DELAY (3200000)
 uint8_t sensor[8];
 char l_encoder[20], r_encoder[20];
 uint8_t sensor_data = 255;
@@ -57,91 +57,91 @@ extern float PID_clear(PID *pid);
 //// and a quarter-angle turn function, 
 //// where the angle is controlled by an encoder.
 
-//float speed_difference_ctrl_value,
-//		gray_bias,
-//		line_patrol_ctrl_value,
-//		line_patrol_weight,
-//		left_ctrl_value,
-//		right_ctrl_value;
+float speed_difference_ctrl_value,
+		gray_bias,
+		line_patrol_ctrl_value,
+		line_patrol_weight,
+		left_ctrl_value,
+		right_ctrl_value;
 
-//typedef enum
-//{
-//	LEFT,
-//	RIGHT
-//} MOTOR_DIRECTION;
-//// This function controls the motor 
-//// to rotate a certain angle by combining the encoder readings. 
-//// It only needs to measure the number of pulses 
-//// for a right-angle turn.
-//void revolve_by_encoder(MOTOR_DIRECTION motor_direction, int delta_decoder)
-//{
-//	int start;
-//	switch (motor_direction)
-//	{
-//	case LEFT:
-//		start = l_encoder_l_count;
-//		while (l_encoder_l_count - start < delta_decoder)
-//		{
-//			Motor_Pwm(BASE_CTRL, 0);
-//		}
-//		break;
-//	case RIGHT:
-//		start = r_encoder_l_count;
-//		while (r_encoder_l_count - start < delta_decoder)
-//		{
-//			Motor_Pwm(0, BASE_CTRL);
-//		}
-//		break;
-//	}
-//	Motor_Pwm(0, 0);
-//}
+typedef enum
+{
+	LEFT,
+	RIGHT
+} MOTOR_DIRECTION;
+// This function controls the motor 
+// to rotate a certain angle by combining the encoder readings. 
+// It only needs to measure the number of pulses 
+// for a right-angle turn.
+void revolve_by_encoder(MOTOR_DIRECTION motor_direction, int delta_decoder)
+{
+	int start;
+	switch (motor_direction)
+	{
+	case LEFT:
+		start = l_encoder_l_count;
+		while (l_encoder_l_count - start < delta_decoder)
+		{
+			Motor_Pwm(BASE_CTRL, 0);
+		}
+		break;
+	case RIGHT:
+		start = r_encoder_l_count;
+		while (r_encoder_l_count - start < delta_decoder)
+		{
+			Motor_Pwm(0, BASE_CTRL);
+		}
+		break;
+	}
+	Motor_Pwm(0, 0);
+}
 
-//char s1[16];
-//extern PID left_wheel_pid,right_wheel_pid;
-//void line_patrol()
-//{
-//	while (1) 
-//	{
-//		sensor_data = gw_gray_serial_read();
-//		gray_bias = gray_calculate_bias(sensor_data);
-//		// Turn right and the left wheel will go faster
-//		// Be positive if you need to turn right
+char s1[16];
+extern PID left_wheel_pid,right_wheel_pid;
+void line_patrol()
+{
+	while (1) 
+	{
+		sensor_data = gw_gray_serial_read();
+		gray_bias = gray_calculate_bias(sensor_data);
+		// Turn right and the left wheel will go faster
+		// Be positive if you need to turn right
 
-//		// Note that both should be 
-//		// positive or negative numbers
-//		float inc_l = PID_calculate(&left_wheel_pid,
-//			l_encoder_speed,
-//			BASE_SPEED + gray_bias * INC_SPEED
-//		);
-//		float inc_r = PID_calculate(&right_wheel_pid,
-//			r_encoder_speed,
-//			BASE_SPEED - gray_bias * INC_SPEED
-//		);
-//		Motor_Pwm(BASE_CTRL + inc_l, BASE_CTRL + inc_r);
-//		//		sprintf(s1,"%06.2f",gray_bias);
-//		//		LCD_clear_L(1, 1);
-//		//		display_6_8_string(1,1,s1);
-//		delay_ms(100);
-//	}
-//}
+		// Note that both should be 
+		// positive or negative numbers
+		float inc_l = PID_calculate(&left_wheel_pid,
+			l_encoder_speed,
+			BASE_SPEED + gray_bias * INC_SPEED
+		);
+		float inc_r = PID_calculate(&right_wheel_pid,
+			r_encoder_speed,
+			BASE_SPEED - gray_bias * INC_SPEED
+		);
+		Motor_Pwm(BASE_CTRL + inc_l, BASE_CTRL + inc_r);
+		//		sprintf(s1,"%06.2f",gray_bias);
+		//		LCD_clear_L(1, 1);
+		//		display_6_8_string(1,1,s1);
+		delay_ms(100);
+	}
+}
 
-//extern void float_items_screen_show(
-//		char* title,
-//		char** item_titles,
-//		float* item_value
-//);
+extern void float_items_screen_show(
+		char* title,
+		char** item_titles,
+		float* item_value
+);
 char buf[50] = {'\0'};
 
 int main(void)
 {
-//	SYSCFG_DL_init();
+	SYSCFG_DL_init();
 
-//	oled_init();
-//	LCD_clear_L(0, 0);
+	oled_init();
+	LCD_clear_L(0, 0);
 
-//	NVIC_EnableIRQ(TIMER_0_INST_INT_IRQN);
-//	NVIC_EnableIRQ(USER_GPIO_INT_IRQN);
-//	DL_TimerG_startCounter(TIMER_0_INST);
+	NVIC_EnableIRQ(TIMER_0_INST_INT_IRQN);
+	NVIC_EnableIRQ(USER_GPIO_INT_IRQN);
+	DL_TimerG_startCounter(TIMER_0_INST);
 	unsigned int LineL1 = 1, LineL2 = 1, LineR1 = 1, LineR2 = 1;//四路巡线模块初始值
 	board_init();//相关初始化
 	sprintf(buf,"Four_way patrol line\n");//四路巡线模块
@@ -170,13 +170,9 @@ int main(void)
 	*/
 	while(1)
 	{
-//		line_patrol();
-//		delay_ms(100);
-			LineR1 = DL_GPIO_readPins(GRAY_PORT, GRAY_X4_PIN) > 0 ? 1 : 0;//读取右一
-			LineR2 = DL_GPIO_readPins(GRAY_PORT, GRAY_X3_PIN) > 0 ? 1 : 0;//读取右二
-			LineL2 = DL_GPIO_readPins(GRAY_PORT, GRAY_X1_PIN) > 0 ? 1 : 0;//读取左二
-			LineL1 = DL_GPIO_readPins(GRAY_PORT, GRAY_X2_PIN) > 0 ? 1 : 0;//读取左一		
-			
+		line_patrol();
+		delay_ms(100);
+		
 			sprintf(buf,"LineL1 = %d,LineL2 = %d,LineR1 = %d,LineR2 = %d\r\n",LineL1,LineL2,LineR1,LineR2);	
 					uart0_send_string(buf);//串口发送函数
 
@@ -185,12 +181,6 @@ int main(void)
 			));		
 			uart0_send_string(buf);//串口发送函数
 		  delay_ms(300);
-		
-		
-		
-		
-		
-		
 		
 		
 		
@@ -205,121 +195,121 @@ int main(void)
 #endif
 }
 
-//void TIMER_0_INST_IRQHandler(void)
-//{
-//	uint32_t l_current_count = l_encoder_l_count;
-//	uint32_t l_steps_this_second = l_current_count - l_last_encoder_count;
-//	l_last_encoder_count = l_current_count;
+void TIMER_0_INST_IRQHandler(void)
+{
+	uint32_t l_current_count = l_encoder_l_count;
+	uint32_t l_steps_this_second = l_current_count - l_last_encoder_count;
+	l_last_encoder_count = l_current_count;
 
-//	l_encoder_speed = l_steps_this_second;
+	l_encoder_speed = l_steps_this_second;
 
-//	uint32_t r_current_count = r_encoder_l_count;
-//	uint32_t r_steps_this_second = r_current_count - r_last_encoder_count;
-//	r_last_encoder_count = r_current_count;
+	uint32_t r_current_count = r_encoder_l_count;
+	uint32_t r_steps_this_second = r_current_count - r_last_encoder_count;
+	r_last_encoder_count = r_current_count;
 
-//	r_encoder_speed = r_steps_this_second;
-//}
+	r_encoder_speed = r_steps_this_second;
+}
 
-//void GROUP1_IRQHandler(void)
-//{
-//	uint8_t x_l, y_l, z_l, sum_l;
-//	uint8_t x_r, y_r, z_r, sum_r;
-//	switch (DL_Interrupt_getPendingGroup(DL_INTERRUPT_GROUP_1))
-//	{
-//	case USER_GPIO_INT_IIDX:
+void GROUP1_IRQHandler(void)
+{
+	uint8_t x_l, y_l, z_l, sum_l;
+	uint8_t x_r, y_r, z_r, sum_r;
+	switch (DL_Interrupt_getPendingGroup(DL_INTERRUPT_GROUP_1))
+	{
+	case USER_GPIO_INT_IIDX:
 
-//		if (DL_GPIO_getEnabledInterruptStatus(GPIOA, USER_GPIO_PULSE_A_PIN))
-//		{
-//			x_l = 1;
+		if (DL_GPIO_getEnabledInterruptStatus(GPIOA, USER_GPIO_PULSE_A_PIN))
+		{
+			x_l = 1;
 
-//			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_A_PIN))
-//				y_l = 1;
-//			else
-//				y_l = 0;
+			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_A_PIN))
+				y_l = 1;
+			else
+				y_l = 0;
 
-//			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_B_PIN))
-//				z_l = 1;
-//			else
-//				z_l = 0;
+			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_B_PIN))
+				z_l = 1;
+			else
+				z_l = 0;
 
-//			sum_l = x_l + y_l + z_l;
-//			if (sum_l == 0 || sum_l == 2)
-//				l_encoder_l_count++;
-//			else
-//				l_encoder_l_count--;
+			sum_l = x_l + y_l + z_l;
+			if (sum_l == 0 || sum_l == 2)
+				l_encoder_l_count++;
+			else
+				l_encoder_l_count--;
 
-//			DL_GPIO_clearInterruptStatus(GPIOA, USER_GPIO_PULSE_A_PIN);
-//		}
+			DL_GPIO_clearInterruptStatus(GPIOA, USER_GPIO_PULSE_A_PIN);
+		}
 
-//		if (DL_GPIO_getEnabledInterruptStatus(GPIOA, USER_GPIO_PULSE_B_PIN))
-//		{
-//			x_l = 0;
+		if (DL_GPIO_getEnabledInterruptStatus(GPIOA, USER_GPIO_PULSE_B_PIN))
+		{
+			x_l = 0;
 
-//			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_A_PIN))
-//				y_l = 1;
-//			else
-//				y_l = 0;
+			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_A_PIN))
+				y_l = 1;
+			else
+				y_l = 0;
 
-//			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_B_PIN))
-//				z_l = 1;
-//			else
-//				z_l = 0;
+			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_B_PIN))
+				z_l = 1;
+			else
+				z_l = 0;
 
-//			sum_l = x_l + y_l + z_l;
-//			if (sum_l == 0 || sum_l == 2)
-//				l_encoder_l_count++;
-//			else
-//				l_encoder_l_count--;
+			sum_l = x_l + y_l + z_l;
+			if (sum_l == 0 || sum_l == 2)
+				l_encoder_l_count++;
+			else
+				l_encoder_l_count--;
 
-//			DL_GPIO_clearInterruptStatus(GPIOA, USER_GPIO_PULSE_B_PIN);
-//		}
+			DL_GPIO_clearInterruptStatus(GPIOA, USER_GPIO_PULSE_B_PIN);
+		}
 
-//		if (DL_GPIO_getEnabledInterruptStatus(GPIOA, USER_GPIO_PULSE_C_PIN))
-//		{
-//			x_r = 1;
+		if (DL_GPIO_getEnabledInterruptStatus(GPIOA, USER_GPIO_PULSE_C_PIN))
+		{
+			x_r = 1;
 
-//			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_C_PIN))
-//				y_r = 1;
-//			else
-//				y_r = 0;
+			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_C_PIN))
+				y_r = 1;
+			else
+				y_r = 0;
 
-//			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_D_PIN))
-//				z_r = 1;
-//			else
-//				z_r = 0;
+			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_D_PIN))
+				z_r = 1;
+			else
+				z_r = 0;
 
-//			sum_r = x_r + y_r + z_r;
-//			if (sum_r == 0 || sum_r == 2)
-//				r_encoder_l_count++;
-//			else
-//				r_encoder_l_count--;
+			sum_r = x_r + y_r + z_r;
+			if (sum_r == 0 || sum_r == 2)
+				r_encoder_l_count++;
+			else
+				r_encoder_l_count--;
 
-//			DL_GPIO_clearInterruptStatus(GPIOA, USER_GPIO_PULSE_C_PIN);
-//		}
+			DL_GPIO_clearInterruptStatus(GPIOA, USER_GPIO_PULSE_C_PIN);
+		}
 
-//		if (DL_GPIO_getEnabledInterruptStatus(GPIOA, USER_GPIO_PULSE_D_PIN))
-//		{
-//			x_r = 0;
+		if (DL_GPIO_getEnabledInterruptStatus(GPIOA, USER_GPIO_PULSE_D_PIN))
+		{
+			x_r = 0;
 
-//			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_C_PIN))
-//				y_r = 1;
-//			else
-//				y_r = 0;
+			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_C_PIN))
+				y_r = 1;
+			else
+				y_r = 0;
 
-//			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_D_PIN))
-//				z_r = 1;
-//			else
-//				z_r = 0;
+			if (DL_GPIO_readPins(GPIOA, USER_GPIO_PULSE_D_PIN))
+				z_r = 1;
+			else
+				z_r = 0;
 
-//			sum_r = x_r + y_r + z_r;
-//			if (sum_r == 0 || sum_r == 2)
-//				r_encoder_l_count++;
-//			else
-//				r_encoder_l_count--;
+			sum_r = x_r + y_r + z_r;
+			if (sum_r == 0 || sum_r == 2)
+				r_encoder_l_count++;
+			else
+				r_encoder_l_count--;
 
-//			DL_GPIO_clearInterruptStatus(GPIOA, USER_GPIO_PULSE_D_PIN);
-//		}
+			DL_GPIO_clearInterruptStatus(GPIOA, USER_GPIO_PULSE_D_PIN);
+		}
 
-//		break;
-//	}
-//}
+		break;
+	}
+}
